6fd83e5c0eda717e7713a904fc276c50e17ba5fe
Hossein3piol <mhsaeidi81@gmail.com>
2023-09-25 23:21:19 +0330

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -82,3 +82,113 @@ sudo apt-get install speedtest
 clear;
 
 ##########      End install speedtest      ############
+
+
+##########      install telegraf      ############
+# influxdata-archive_compat.key GPG fingerprint:
+#     9D53 9D90 D332 8DC7 D6C8 D3B9 D8FF 8E1F 7DF8 B07E
+wget -q https://repos.influxdata.com/influxdata-archive_compat.key
+echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
+
+sudo apt-get update && sudo apt-get install telegraf -y
+clear;
+
+mv /etc/telegraf/telegraf.conf /etc/telegraf/telegraf.conf-org
+
+sudo tee /etc/telegraf/telegraf.conf<<EOF
+[agent]
+  interval = "10s"
+  round_interval = true
+  metric_batch_size = 1000
+  metric_buffer_limit = 10000
+  collection_jitter = "0s"
+  flush_interval = "10s"
+  flush_jitter = "0s"
+  precision = ""
+  hostname = ""
+  omit_hostname = false
+[[outputs.influxdb]]
+ urls = ["http://localhost:8086"]
+ database = "part_db"
+ username = "admin"
+ password = "admin"
+
+[[outputs.prometheus_client]]
+  metric_version = 2
+  listen = ":9273"
+[[inputs.cpu]]
+  percpu = true
+  totalcpu = true
+  collect_cpu_time = false
+  report_active = false
+[[inputs.disk]]
+  ignore_fs = ["tmpfs", "devtmpfs", "devfs", "iso9660", "overlay", "aufs", "squashfs"]
+  mount_points = ["/"]
+[[inputs.diskio]]
+[[inputs.mem]]
+[[inputs.net]]
+[[inputs.processes]]
+[[inputs.swap]]
+[[inputs.system]]
+EOF
+
+service telegraf restart
+
+
+##########      end install telegraf      ############
+
+##########      install influxDB      ############
+
+# influxdata-archive_compat.key GPG Fingerprint: 9D539D90D3328DC7D6C8D3B9D8FF8E1F7DF8B07E
+wget -q https://repos.influxdata.com/influxdata-archive_compat.key
+echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
+
+sudo apt-get update && sudo apt-get install influxdb -y
+sudo service influxdb start
+
+
+influx -execute CREATE USER admin WITH PASSWORD 'admin' WITH ALL PRIVILEGES 
+
+# Define the regular expression pattern to search for
+SEARCH_PATTERN="# auth-enabled = false"
+
+# Define the replacement text
+REPLACE_TEXT="auth-enabled = true"
+
+# Use sed to perform the replacement using regular expressions
+sed -i "s/$SEARCH_PATTERN/$REPLACE_TEXT/" /etc/influxdb/influxdb.conf
+
+sudo systemctl restart influxdb
+
+influx -username 'admin' -password 'admin' -execute CREATE DATABASE part_db 
+
+
+##########      end install influxDB      ############
+
+##########      install prometheus      ############
+
+
+
+
+
+
+##########      end install prometheus      ############
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
