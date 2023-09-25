f664ca5965a006da9feaf2694d57de9d85c8d486
Hossein3piol <mhsaeidi81@gmail.com>
2023-09-25 22:40:27 +0330

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -54,7 +54,7 @@ sudo mkdir -p /etc/apt/keyrings/
 sudo wget -q -O /etc/apt/keyrings/grafana.key https://apt.grafana.com/gpg.key
 #wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
 #To add a repository for stable and beta releases
-echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+echo "deb [signed-by=/etc/apt/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 #echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 # Updates the list of available packages
 sudo apt-get update
