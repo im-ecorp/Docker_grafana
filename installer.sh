feaa472d1a1fbf15abb06f123bc184227657ed55
Hossein3piol <mhsaeidi81@gmail.com>
2023-09-27 04:47:42 +0330

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -47,13 +47,7 @@ sudo service grafana-server start
 clear;
 #############################    End of install grafana      ################################################
 
-##########      install speedtest      ############
-sudo apt-get install curl -y
-curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
-sudo apt-get install speedtest
-clear;
 
-##########      End install speedtest      ############
 
 
 ##########      install telegraf      ############
